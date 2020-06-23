e7003ff3a7899357c3a01e962e86f76f94c19c72
martin jerez <martincho111986@gmail.com>
2020-06-23 01:36:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// VARIABLES
+
+// let
+// const
+
+// let nombre = 'santiago';
+// nombre = 'martin';
+
+// const nombre = 'santiago';
+
+
+// const numero1 = 100;
+// const numero2 = 200;
+
+// const resultado = numero1 + numero2
+
+// const arreglo = [1,2,3,4,5,6,7,8];
+
+// arreglo.push(1)
+
+// const persona = {
+//     nombre: 'santiago',
+//     altura: '1:83',
+//     apellido: 'vies'
+// }
+
+// console.log(persona)
+// document.write(persona)
+
+// alert('hello world')
+// console.log('hello world')
+// document.write('hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
